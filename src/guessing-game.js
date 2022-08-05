924d4e9e550608312928e102fc9117e813d654fb
Artem Pushkarev <pushkarev_av@inbox.ru>
2022-08-05 14:36:47 +0300

    add: code in guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let mid = Math.round((this.max - this.min) / 2 + this.min);
+        this.mid = mid;
+        return mid;
     }
-
+    
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
